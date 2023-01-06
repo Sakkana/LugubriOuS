ef34028c7044da68b1d12ce9414c47cb4c78473e
Sakkana <642835027@qq.com>
2023-01-06 21:32:52 +0800

    change clock frequency ok

--- src/kernel/init.c
+++ src/kernel/init.c
@@ -1,9 +1,12 @@
 #include "init.h"
-#include "interrupt.h"
 #include "print.h"
+#include "interrupt.h"
+#include "../device/timer.h"
 
-// 初始化所有模块
+/*负责初始化所有模块 */
 void init_all() {
-    put_str("init_all\n");
-    idt_init();
+   put_str("init_all\n");
+   idt_init();	     // 初始化中断
+   timer_init();     // 初始化定时器
 }
+
